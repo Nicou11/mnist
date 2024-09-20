6b51a632eadff2afa7ab8e668428485eadb4ff24
Nicou11 <lyoungin1204@gmail.com>
2024-09-20 15:34:25 +0900

    add, one, many

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -1,7 +1,10 @@
 from typing import Annotated
 from fastapi import FastAPI, File, UploadFile
+from datetime import datetime
+from jigutime import jigu
 import os
 import pymysql.cursors
+import pytz
 
 app = FastAPI()
 
@@ -9,38 +12,53 @@ app = FastAPI()
 def test():
     return {"Test": "Done"}
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
+@app.get("/files")
+async def file_list():
+    conn = pymysql.connect(host='172.18.0.1', port = 53306,
+                            user = 'mnist', password = '1234',
+                            database = 'mnistdb',
+                            cursorclass=pymysql.cursors.DictCursor)
+    with conn:
+        with conn.cursor() as cursor:
+            sql = "SELECT * FROM image_processing WHERE prediction_time IS NULL ORDER BY num"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+    return result
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     # 파일 저장
+    korea = datetime.now(pytz.timezone('Asia/Seoul'))
+    request_time = korea.strftime('%Y-%m-%d %H:%M:%S')
+
     img = await file.read()
     file_name = file.filename
+    file_ext = file.content_type.split('/')[-1]  #"image/png"
     # 디렉토리가 없으면 오류, 코드에서 확인 및 만들기 추가
-    upload_dir = "./photo"
-    ffpath = os.path.join(upload_dir, file_name)
+    upload_dir = "/home/young12/code/mnist/img"
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    import uuid
+    ffpath = os.path.join(upload_dir, f'{uuid.uuid4()}.{file_ext}')
 
-    import pymysql.cursors
+    with open(ffpath, "wb") as f:
+        f.write(img)
 
-    connection = pymysql.connect(host=os.getenv("DB_IP", "localhost"),
-                        port= int(os.getenv("DB_PORT", "33306")),
+    con = pymysql.connect(host="172.17.0.1",
+                        port= 53306,
                         user='mnist',
                         password='1234',
                         db='mnistdb',
                         cursorclass=pymysql.cursors.DictCursor)
 
-    sql = "INSERT INTO image_processing (file_name, content_type, dt) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO image_processing (file_name, file_path, request_time, request_user) VALUES (%s, %s, %s, %s)"
 
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(sql, (file_name, content_type, real_time))
-        connection.commit()
+    with con:
+        with con.cursor() as cursor:
+            cursor.execute(sql, (file_name, ffpath, jigu.now(), "n06"))
+        con.commit()
 
-    with open(ffpath, "wb") as f:
-        f.write(img)
 
     # 파일 저장 경로 DB INSERT
     # table name : image_processing
@@ -49,7 +67,37 @@ async def create_upload_file(file: UploadFile):
     # 컬럼 정보 : 예측 모델, 예측 결과, 예측 시간(추후 업데이트)
 
     return {
-            "filename": file.filename,
+            "filename": file_name,
             "content_type": file.content_type,
-            "file_full_path": ffpath,
+            "file_path": ffpath,
             }
+
+@app.get("/all")
+def all():
+    # DB 연결 SELECT ALL
+    # 결과값 리턴
+    from mnist.db import select
+    sql = "SELECT * FROM image_processing"
+    result = select(query=sql, size=-1)
+    return result
+
+@app.get("/one")
+def one():
+    # DB 연결 SELECT 값 중 하나만 리턴
+    # 결과값 리턴
+    sql = """SELECT * FROM image_processing WHERE prediction_time IS NULL ORDER BY num LIMIT 1"""
+    result = select(query=sql, size=1)
+    return result[0] 
+
+@app.get("/many/")
+def many(size: int = -1):
+    
+    con = pymysql.connect(host="172.17.0.1",                                                               port= 53306,                                                                       user='mnist',                                                                      password='1234',                                                                   db='mnistdb',                                                                      cursorclass=pymysql.cursors.DictCursor)
+    sql = "SELECT * FROM image_processing WHERE prediction_time IS NULL ORDER BY num"
+    
+    with con:
+        with con.cursor() as cursor:
+            cursor.execute(sql)
+            result = cursor.fetchmany(size)
+        
+    return result
