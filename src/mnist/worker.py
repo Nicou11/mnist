71f68ef81a576b318498a0c1a4b6ce59c68e619d
Nicou11 <lyoungin1204@gmail.com>
2024-09-23 12:11:27 +0900

    worker

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -6,16 +6,7 @@ def run():
 
   # STEP 1
   # image_processing 테이블의 prediction_result IS NULL 인 ROW 1 개 조회 - num 갖여오기
-    sql = "
-    SELECT * 
-    FROM image_processing
-    WHERE prediction_result IS NULL
-    ORDER BY num 
-    LIMIT 1
-    "
 
-    result = select(query=sql, size=1)
-    return result[0]
   # STEP 2
   # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
   # 동시에 prediction_model, prediction_time 도 업데이트
